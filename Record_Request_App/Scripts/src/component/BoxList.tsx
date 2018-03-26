eeef66ef328d86cf80ef71f4ebd41d0d1da758ff
Joshua-Jenkins33 <joshuabyu@yahoo.com>
2018-03-26 14:00:00 -0600

    tyler made it kosher.  added styling, more efficient

--- Record_Request_App/Scripts/src/component/BoxList.tsx
+++ Record_Request_App/Scripts/src/component/BoxList.tsx
@@ -1,90 +1,96 @@
 /* tslint:disable:no-unused-variable */
-import * as React from 'react';
+import * as React from 'react'
 /* tslint:enable:no-unused-variable */
-import { TextField } from 'office-ui-fabric-react/lib/TextField';
+import { TextField } from 'office-ui-fabric-react/lib/TextField'
 import {
   DetailsList,
   DetailsListLayoutMode,
   Selection,
   IColumn,
-  CheckboxVisibility,
-} from 'office-ui-fabric-react/lib/DetailsList';
-import { MarqueeSelection } from 'office-ui-fabric-react/lib/MarqueeSelection';
-import { autobind } from 'office-ui-fabric-react/lib/Utilities';
-import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
+  CheckboxVisibility
+} from 'office-ui-fabric-react/lib/DetailsList'
+import { MarqueeSelection } from 'office-ui-fabric-react/lib/MarqueeSelection'
+import { autobind } from 'office-ui-fabric-react/lib/Utilities'
+import { initializeIcons } from 'office-ui-fabric-react/lib/Icons'
 
 // Enables microsoft ui icons to appear
 initializeIcons()
 
+// Styling
+
+const squeezeTable = {
+  marginTop: '2.5%',
+  marginLeft: '25%',
+  marginRight: '25%'
+} as React.CSSProperties
+
+const Links = {
+  color: "#0078d7"
+  
+} as React.CSSProperties
+
 export interface iBoxList {
-    boxData,
-    currentDep,
+  boxData
+  currentDep
 }
 
-const _items: {}[] = [];
+const _items: {}[] = []
 
 const _columns: IColumn[] = [
   {
     key: 'column1',
-    name: 'Name',
-    fieldName: 'name',
+    name: 'Box Number',
+    fieldName: 'boxNumber',
     minWidth: 100,
-    maxWidth: 200,
+    maxWidth: 250,
     isResizable: true,
     ariaLabel: 'Operations for name'
   },
   {
     key: 'column2',
-    name: 'Value',
-    fieldName: 'value',
+    name: '',
+    fieldName: 'checkoutBox',
     minWidth: 100,
-    maxWidth: 200,
+    maxWidth: 250,
     isResizable: true,
-    ariaLabel: 'Operations for value'
+    ariaLabel: 'Operations for checkoutBox'
   },
-];
+  {
+    key: 'column3',
+    name: '',
+    fieldName: 'viewFolders',
+    minWidth: 100,
+    maxWidth: 250,
+    isResizable: true,
+    ariaLabel: 'Operations for viewFolders'
+  }
+]
 
-export function BoxList (props: iBoxList) {
+export function BoxList(props: iBoxList) {
+  console.log(props.boxData.filter((x, i) => x.DepId == props.currentDep))
+  const bIdList = props.boxData
+    .filter((x, i) => x.DepId == props.currentDep)
+    .map((x, i) => ({
+      key: i,
+      boxNumber: <p className="ms-fontSize-mPlus ms-fontWeight-light">{'B' + x.BoxIdBarCode.toString()}</p>,
+      checkoutBox: <p style={Links} className="ms-fontSize-mPlus ms-fontWeight-light">+ Add Box</p>,
+      viewFolders: <p style={Links} className="ms-fontSize-mPlus ms-fontWeight-light">View Folders</p>
+    }))
 
-    const bIdList = []
-    const test = []
+  // Grab just the BoxIdBarCodes, turn them into a string so I can add a "B" onto it
+  // This is how it is stored in the ROC, (as an int with a function adding on a B), so this is why it's done this way
+  // Also checks to make sure we are grabbing boxes within the selected department
 
-
-    props.boxData.forEach(x => { if(x.DepId == props.currentDep) {
-      bIdList.push("B" + x.BoxIdBarCode.toString())
-    }})
-
-    // Grab just the BoxIdBarCodes, turn them into a string so I can add a "B" onto it
-    // This is how it is stored in the ROC, (as an int with a function adding on a B), so this is why it's done this way
-
-    if (_items.length === 0) {
-      bIdList.forEach((x,i) => {
-          _items.push({
-              key: i,
-              name: `Box Number ${x}`,
-              value: i,
-          });
-      })
-    }
-
-  {console.log(props.currentDep)}
-  {console.log(bIdList)}
-
-    return (
-      <div>
-        <TextField
-          label='Filter by BoxId:'
-        />
-          <DetailsList
-            items={ bIdList }
-            columns={ _columns }
-            setKey='set'
-            layoutMode={ DetailsListLayoutMode.fixedColumns }
-            checkboxVisibility={ CheckboxVisibility.hidden }
-
-          />
-      </div>
-    );
-
-
+  return (
+    <div style={squeezeTable}>
+      <TextField label="Filter by BoxId:" />
+      <DetailsList
+        items={bIdList}
+        columns={_columns}
+        setKey="set"
+        layoutMode={DetailsListLayoutMode.fixedColumns}
+        checkboxVisibility={CheckboxVisibility.hidden}
+      />
+    </div>
+  )
 }
