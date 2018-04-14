ff5503a2d81d15e2e2ba8f6772f73991b9229c1f
Joshua-Jenkins33 <joshuabyu@yahoo.com>
2018-04-13 23:34:27 -0600

    added folderData to the application

--- Record_Request_App/Scripts/src/main.tsx
+++ Record_Request_App/Scripts/src/main.tsx
@@ -3,7 +3,8 @@ import * as ReactDom from "react-dom";
 import { BrowserRouter, Router } from "react-router-dom";
 import { App } from "./component/App";
 import { mockUser } from "./res/mockuser";
-import { boxData } from './res/boxdata'
+import { boxData } from './res/boxdata';
+import { folderData } from './res/folderdata';
 
 // in strict mode, mobx requires that all observable data members only be modified through explicit @action mutators
 
@@ -12,4 +13,5 @@ const root = document.getElementById("root");
 ReactDom.render(<App
                 user={mockUser}
                 boxData={boxData}
+                folderData={folderData}
                 />, root);
