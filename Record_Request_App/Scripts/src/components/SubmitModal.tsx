46b1eb189559f9772ce4e148efe60098ac9b5231
Joshua-Jenkins33 <joshuabyu@yahoo.com>
2018-05-11 16:56:22 -0600

    added methods and parameters to submit modal, now functions

--- Record_Request_App/Scripts/src/components/SubmitModal.tsx
+++ Record_Request_App/Scripts/src/components/SubmitModal.tsx
@@ -2,23 +2,18 @@ import * as React from 'react'
 import { Modal } from 'office-ui-fabric-react/lib/Modal'
 import { DefaultButton, PrimaryButton } from 'office-ui-fabric-react/lib/Button'
 import { IFolderData, IBoxData } from '../models/MockData'
-
 import { TextField } from 'office-ui-fabric-react'
 import { Toggle } from 'office-ui-fabric-react'
 import { IFolderAndBox } from '../models/App'
 
-// Styling
-
-const Links = {
-  color: '#0078d7',
-  cursor: 'pointer'
-} as React.CSSProperties
-
 export interface ISubmitModal {
   close(): void
-  requestType: boolean
   selectedItems: Map<number, IFolderAndBox>
   submit(x): any
+  updateInstructions(e): void
+  priority(): void
+  requestType(): void
+  deliveryInstructions: string
 }
 
 // ----------------------------------------------
@@ -33,18 +28,16 @@ export function SubmitModal(props: ISubmitModal) {
         containerClassName="ms-modalExample-container"
       >
         <div className="ms-modalExample-header">
-          {!!props.requestType ? <h1>Box Request</h1> : <h1>Folder Request</h1>}
+          <h1>Item Request</h1>
         </div>
         <div>
           <Toggle
-            // checked={ props.isChecked }
             label="Request Type"
             onAriaLabel="This toggle is checked. Press to uncheck."
             offAriaLabel="This toggle is unchecked. Press to check."
             onText="Permanent"
             offText="Temporary"
-            // onChanged={props.openWarning}
-            // onChanged={props.makeToggle}
+            onChanged={props.requestType}
           />
           <Toggle
             // checked={props.isChecked}
@@ -53,15 +46,19 @@ export function SubmitModal(props: ISubmitModal) {
             offAriaLabel="This toggle is unchecked. Press to check."
             onText="Urgent"
             offText="Standard"
-            // onChanged={props.openWarning}
-            // onChanged={props.makeToggle}
+            onChanged={props.priority}
           />
-          <TextField multiline={true} label="Delivery Instructions" />
+          <TextField
+            multiline={true}
+            label="Delivery Instructions"
+            value={props.deliveryInstructions}
+            onChanged={props.updateInstructions}
+          />
         </div>
         <div>
           <PrimaryButton
             text="Submit"
-            onClick={props.submit(props.selectedItems)}
+            onClick={(x) => props.submit(props.selectedItems)}
           />
           <PrimaryButton text="Cancel" onClick={props.close} />
         </div>
