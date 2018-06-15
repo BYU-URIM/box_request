c6ce800142b99f1d8ee94ebb133f2eed2d57f5e5
tgilland95 <tylergilland@gmail.com>
2018-06-15 01:44:05 -0600

    async render to initialize stores before page paint, inject dataservice

--- Record_Request_App/Scripts/src/main.tsx
+++ Record_Request_App/Scripts/src/main.tsx
@@ -1,19 +1,23 @@
 import * as React from "react"
 import * as ReactDom from "react-dom"
-import { BrowserRouter, Router } from "react-router-dom"
 import { App } from "./components/"
-import { folderData, mockUser, boxData } from "./res/"
-import { DataService } from "./services/DataService"
 import { Provider } from "mobx-react"
-import { rootStore } from "./stores/RootStore"
+import { DataServiceFactory } from "./services/DataServiceFactory"
+import { useStrict } from "mobx"
+
+useStrict(true)
+
 const root = document.getElementById("root")
-
+const rootStore = DataServiceFactory.getRootStore()
 window["rootStore"] = rootStore
-
-
-ReactDom.render(
-    <Provider rootStore={rootStore} >
-        <App />
-    </Provider>,
-    root
-)
+const init = async () => {
+    await rootStore.init()
+    if (rootStore.initialized)
+        ReactDom.render(
+            <Provider rootStore={rootStore}>
+                <App />
+            </Provider>,
+            root
+        )
+}
+init()
