17fc75e302de8a8bfa67b335e4af5b8710045a3f
tgilland95 <tylergilland@gmail.com>
2018-06-18 11:44:59 -0600

    add in request state, folderform, and more stuff to split up stores

--- Record_Request_App/Scripts/src/stores/RequestStore/RequestStore.ts
+++ Record_Request_App/Scripts/src/stores/RequestStore/RequestStore.ts
@@ -1,77 +1,55 @@
 import { action, observable, computed, ObservableMap } from "mobx"
-import { IBoxDataArrObj, IFolderDataArrObj } from "../../res"
-import { IBoxDataObj, IFolderDataObj } from "../../models/MockData"
-import { ModalTypes, IFolderAndBox, CheckoutTypes } from "../../models"
+import { IBoxArr, IFolderArr } from "../../res"
+import { IBox, IFolder, IFolderOrBox } from "../../models/StoreModels"
+import { ModalTypes } from "../../models"
 import { RootStore } from "../RootStore/RootStore"
 import { FolderForm } from "./FolderForm"
+import { RequestState } from "./RequestState"
 export class RequestStore {
-    boxes: IBoxDataArrObj = undefined
-    folders: IFolderDataArrObj = undefined
-    folderForm: FolderForm
-    _selectedItems: ObservableMap<IFolderAndBox> = observable.map()
+    boxes: IBoxArr
+    folders: IFolderArr
 
-    @observable selectedBox?: IBoxDataObj
-    @observable modal: ModalTypes = ModalTypes.none
-    @observable selectedDepartment: number = 0
-
+    @observable folderForm: FolderForm
+    @observable requestState: RequestState
     @observable deliveryInstructions: string = ""
     @observable checkoutCart: any
     @observable requestIsUrgent: boolean = false
     @observable requestIsPermanent: boolean = false
 
-    constructor(
-        folders: IFolderDataArrObj,
-        boxes: IBoxDataArrObj,
-        private root: RootStore
-    ) {
+    constructor(folders: IFolderArr, boxes: IBoxArr, private root: RootStore) {
         this.boxes = boxes
         this.folders = folders
     }
 
     init = async () => {
-        return
-    }
-
-    @computed
-    get selectedItems(): Array<IFolderAndBox> {
-        return Array.from(this._selectedItems.values())
-    }
-
-    @computed
-    get filteredBoxes(): Array<IBoxDataObj> {
-        return this.boxes.filter(box => box.DepId === this.selectedDepartment)
-    }
-
-    @computed
-    get filteredFolders(): Array<IFolderDataObj> {
-        return this.folders.filter(
-            (folder: IFolderDataObj) =>
-                folder.BoxID === this.selectedBox.BoxIdBarCode
+        this.requestState = await new RequestState(
+            this,
+            this.folders,
+            this.boxes
         )
+        this.requestState.department = 5556
+        this.requestState.box = this.boxes[1]
+        this.requestState.folder = this.folders[0]
+        this.requestState.addToCart(this.requestState.boxes[0])
+        this.requestState.modal = ModalTypes.submit
+        return
     }
 
     @computed
     get filteredFolderNames(): Array<string> {
         return this.folders
             .filter(
-                (folder: IFolderDataObj) =>
-                    folder.BoxID === this.selectedBox.BoxIdBarCode
+                (folder: IFolder) =>
+                    folder.BoxID === this.requestState.box.BoxIdBarCode
             )
             .map(folder => folder.FolderName.toLowerCase())
     }
 
     @computed
     get checkoutFolderBoxIds(): Array<number> {
-        return Array.from(this.selectedItems.values())
-            .filter(item => item.BoxID)
-            .map(folder => folder.BoxID)
-    }
-
-    @computed
-    get selectedBoxInCheckout(): boolean {
-        return this.selectedBox
-            ? this.itemInCheckout(this.selectedBox.BoxIdBarCode.toString())
-            : false
+        return Array.from(this.requestState.cart.values())
+            .filter(item => (item as IFolder).BoxID)
+            .map(folder => (folder as IFolder).BoxID)
     }
 
     // Not currently using
@@ -98,85 +76,49 @@ export class RequestStore {
         this.deliveryInstructions = instructionInput
     }
 
-    getBox = (boxIdBarCode: number): IBoxDataObj =>
-        this.boxes.find(box => box.BoxIdBarCode === boxIdBarCode)
+    @action
+    initializeFolderForm = (): void => {
+        this.folderForm = new FolderForm(this.filteredFolderNames)
+    }
 
     @action
     createFolder = (): void => {
         this.folders.push({
-            BoxID: this.selectedBox.BoxIdBarCode,
+            BoxID: this.requestState.box.BoxIdBarCode,
             FolderName: this.folderForm.folderName,
             FolderIdBarCode: this.folders.length + 1,
-            Location: this.selectedBox.Location,
+            Location: this.requestState.box.Location,
             Folder_Description: "",
         })
         console.log(this.folders)
     }
 
     @action
-    addItemToCheckout = (item: IFolderAndBox) => {
-        item.FolderIdBarCode
-            ? this._selectedItems.set(item.FolderIdBarCode.toString(), item)
-            : this._selectedItems.set(item.BoxIdBarCode.toString(), item)
+    submitRequest = () => {
+        console.log(this.requestState.cart)
+        this.requestState.clearCart()
+        this.requestState.modal = ModalTypes.none
     }
 
     @action
-    submitRequest = (items: Map<number, IFolderAndBox>) => {
-        this._selectedItems.forEach(item => {
-            item.Location = String(this.selectedDepartment)
-            console.log(this.determineCheckoutType(item))
-        })
-        console.log(this.selectedDepartment)
-        this._selectedItems.clear()
-        this.setModalType(ModalTypes.none)
+    determineCheckoutType = (item: IFolderOrBox): string => {
+        return this.canAddItem(item)
+            ? "+ Add Item to Checkout"
+            : this.inYourPossession(item)
+                ? "- In Your Possession"
+                : "- Item Not Available"
     }
 
-    @action
-    setSelectedBox = (_selectedBox: IBoxDataObj) => {
-        this.selectedBox = _selectedBox
-    }
-
-    @action
-    setModalType = (_modal: ModalTypes) => {
-        if (_modal === ModalTypes.create) {
-            this.folderForm = new FolderForm(this.filteredFolderNames)
-        }
-        this.modal = _modal
-    }
-
-    @action
-    chooseDepartment = (depId: number) => {
-        this.selectedDepartment = depId
-        this.setSelectedBox(undefined)
-    }
-    @action
-    removeItemFromCheckout = (itemNum: string) => {
-        this._selectedItems.delete(itemNum)
-    }
-
-    @action
-    itemInCheckout = (itemNum: string): boolean =>
-        this._selectedItems.has(itemNum)
-
-    // Not working when submitting folders
-    @action
-    determineCheckoutType = (item: IFolderAndBox): string => {
-        let checkoutStatus: CheckoutTypes = CheckoutTypes.none
-        let status = ""
-
-        if (
-            item.Location.charAt(0) === "L" ||
+    canAddItem = (item: IFolderOrBox): boolean => {
+        return (
+            item.Location.startsWith("L") ||
             item.BoxID === Number(item.Location)
-        ) {
-            checkoutStatus = CheckoutTypes.none
-            status = "+ Add Item to Checkout"
-        } else if (item.Location === String(this.selectedDepartment)) {
-            checkoutStatus = CheckoutTypes.depPossession
-            status = "- In Your Possession"
-        } else {
-            checkoutStatus = CheckoutTypes.notAvailable
-            status = "- Item Not Available"
-        }
-        return status
+        )
     }
+    inYourPossession = (item: IFolderOrBox): boolean => {
+        return item.Location === String(this.requestState.department)
+    }
+    @action
+    itemInCheckout = (item: IFolderOrBox): boolean =>
+        this.requestState.cartContains(item)
 }
