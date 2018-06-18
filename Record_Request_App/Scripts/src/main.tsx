9299c91a27fec9fd44b1396936bd16c2dad5c614
tgilland95 <tylergilland@gmail.com>
2018-06-18 11:41:47 -0600

    mobx updates

--- Record_Request_App/Scripts/src/main.tsx
+++ Record_Request_App/Scripts/src/main.tsx
@@ -3,9 +3,6 @@ import * as ReactDom from "react-dom"
 import { App } from "./components/"
 import { Provider } from "mobx-react"
 import { DataServiceFactory } from "./services/DataServiceFactory"
-import { useStrict } from "mobx"
-
-useStrict(true)
 
 const root = document.getElementById("root")
 const rootStore = DataServiceFactory.getRootStore()
