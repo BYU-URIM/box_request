46bf6283ae2422c43a451cc9db46215154322ab5
tgilland95 <tylergilland@gmail.com>
2018-06-20 14:53:58 -0600

    added right classes into gretting

--- Record_Request_App/Scripts/src/components/Greeting/Greeting.tsx
+++ Record_Request_App/Scripts/src/components/Greeting/Greeting.tsx
@@ -1,13 +1,20 @@
 import * as React from "react"
 import "./styles.scss"
+import { PrimaryButton } from "office-ui-fabric-react"
+import { AppModes } from "../../stores/SessionStore/SessionStore"
 
 export interface IGreeting {
     name: string
     departmentid: number[]
+    switchApp(mode: AppModes): void
 }
 
 export const Greeting = (props: IGreeting) => (
-    <div className={"greeting-wrapper"}>
-        <h2 className={"ms-font-l"}> Welcome, {props.name}</h2>
+    <div className={"ms-Grid-row"}>
+        <div className={"ms-Grid-col ms-sm4 ms-smPush8"}>
+            <div className={"greeting-wrapper"}>
+                <h2 className={"ms-font-l"}> Welcome, {props.name}</h2>
+            </div>
+        </div>
     </div>
 )
