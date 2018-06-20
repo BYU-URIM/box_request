880b74899145d3275a2fc964c0dc8b0829d2b6fb
tgilland95 <tylergilland@gmail.com>
2018-06-20 14:51:54 -0600

    added appselector component

---
+++ Record_Request_App/Scripts/src/components/AppSelector/AppSelector.tsx
@@ -0,0 +1,21 @@
+import React = require("react")
+import { PrimaryButton } from "office-ui-fabric-react"
+import { AppModes } from "../../stores/SessionStore/SessionStore"
+import "./styles.scss"
+export interface IAppSelectorProps {
+    switchApp(mode: AppModes): void
+}
+export const AppSelector = (props: IAppSelectorProps) => {
+    return (
+        <div className={"ms-Grid-row appselector-wrapper"}>
+            <PrimaryButton
+                text={"Box Request"}
+                onClick={() => props.switchApp("BoxRequest")}
+            />
+            <PrimaryButton
+                text={"Box Creator"}
+                onClick={() => props.switchApp("BoxCreate")}
+            />
+        </div>
+    )
+}
