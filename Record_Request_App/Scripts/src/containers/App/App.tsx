80919e29446d40a39e6a1a1a44a9aab45d6dd137
tgilland95 <tylergilland@gmail.com>
2018-06-20 14:54:28 -0600

    simplified App and set it up for conditional logic

--- Record_Request_App/Scripts/src/containers/App/App.tsx
+++ Record_Request_App/Scripts/src/containers/App/App.tsx
@@ -1,65 +1,37 @@
 import * as React from "react"
 import { initializeIcons } from "@uifabric/icons"
-import { DepartmentDropdown, Greeting } from "../../components"
-import { inject, observer } from "mobx-react"
-import { RootStore } from "../../stores/RootStore/RootStore"
-import { Fabric } from "office-ui-fabric-react"
 import "./styles.scss"
-import { RequestBuilder } from "../RequestBuilder/RequestBuilder"
+import { Fabric, PrimaryButton } from "office-ui-fabric-react"
+import { BoxRequest } from "../BoxRequest/BoxRequest"
+import { SessionStore } from "../../stores/SessionStore/SessionStore"
+import { inject, observer } from "mobx-react"
+import { BoxCreator } from "../BoxCreator/BoxCreator"
+import { Greeting, AppSelector } from "../../components"
 
 initializeIcons()
 @inject("rootStore")
 @observer
 export class App extends React.Component<any, any> {
-    rootStore: RootStore
-
+    sessionStore: SessionStore
+    componentWillMount() {
+        this.sessionStore = this.props.rootStore.sessionStore
+    }
     render() {
-        this.rootStore = this.props.rootStore
-        const { requestStore } = this.rootStore
-        const { currentUser } = this.rootStore.sessionStore
-
         return (
             <Fabric>
                 <div className={"ms-Grid"}>
-                    <div className={"ms-Grid-row"}>
-                        <div className={"ms-Grid-col ms-sm4 ms-smPush8"}>
-                            <Greeting
-                                name={currentUser.name}
-                                departmentid={currentUser.departments}
-                            />
-                        </div>
-                    </div>
-                    <div className={"ms-Grid-row"}>
-                        <div className={"ms-Grid-col ms-sm4 ms-smPush4"}>
-                            <DepartmentDropdown
-                                mockUser={currentUser}
-                                mockData={requestStore.boxes}
-                                changeSelectedDep={(department: number) =>
-                                    (requestStore.requestState.department = department)
-                                }
-                            />
-                        </div>
-                    </div>
-
-                    <div className={"ms-Grid-row"}>
-                        <RequestBuilder
-                            folderForm={requestStore.folderForm}
-                            requestForm={requestStore.requestForm}
-                            requestState={requestStore.requestState}
-                            requestIsUrgent={requestStore.requestIsUrgent}
-                            updateDeliveryInstructions={
-                                requestStore.updateDeliveryInstructions
-                            }
-                            updateIsUrgent={requestStore.updateIsUrgent}
-                            updateIsPermament={requestStore.updateIsPermanent}
-                            submitRequest={requestStore.submitRequest}
-                            createFolder={requestStore.createFolder}
-                            initializeFolderForm={requestStore.initializeFolderForm}
-                            initializeRequestForm={requestStore.initializeRequestForm}
-                            determineCheckoutType={
-                                requestStore.determineCheckoutType}
-                        />
-                    </div>
+                    <Greeting
+                        name={this.sessionStore.currentUser.name}
+                        departmentid={this.sessionStore.currentUser.departments}
+                        switchApp={this.sessionStore.switchApp}
+                    />
+                    <AppSelector switchApp={this.sessionStore.switchApp} />
+                    {this.sessionStore.appMode === "BoxRequest" && (
+                        <BoxRequest />
+                    )}
+                    {this.sessionStore.appMode === "BoxCreate" && (
+                        <BoxCreator />
+                    )}
                 </div>
             </Fabric>
         )
