482c5022843c2621a36f4f5dcc26cd1ef91cd401
URIM Developer <joshuabyu@yahoo.com>
2018-06-28 15:36:24 -0600

    simplified dropdown component

--- Record_Request_App/Scripts/src/components/DepartmentDropdown/DepartmentDropdown.tsx
+++ Record_Request_App/Scripts/src/components/DepartmentDropdown/DepartmentDropdown.tsx
@@ -8,35 +8,32 @@ import { observer } from "mobx-react"
 import { RequestState } from "../../stores/RequestStore/RequestState";
 
 export interface IUserDeps {
-    mockUser
-    mockData
     changeSelectedDep(val: number)
     title: string
     requestState: RequestState
+    disabled: boolean
+    selectedKey: number
 }
 
 export const DepartmentDropdown = observer((props: IUserDeps) => {
-    const deps = []
-    const depsid = []
-    const departmentObj = []
-    const userDepartments = props.mockUser.departments
-    const departmentInfo = props.mockData
 
     let userDeps = props.requestState.userDepartments
-
     return (
         <div>
             <h1 className="ms-font-xxl department-dropdown-center">
                 {props.title}
             </h1>
-
             <Dropdown
                 placeHolder="Departments"
                 options={userDeps.map((department, i) => {
                     return { key: userDeps[i].id, text: `${userDeps[i].id} - ${userDeps[i].name}`, value: "not a value" }
                 })}
-                onChanged={department => props.changeSelectedDep(Number(department.key))}                
+                onChanged={department => props.changeSelectedDep(Number(department.key))}   
+                id={"test"} 
+                disabled={props.disabled}
+                defaultSelectedKey={props.selectedKey}
             />
+
         </div>
     )
 })
