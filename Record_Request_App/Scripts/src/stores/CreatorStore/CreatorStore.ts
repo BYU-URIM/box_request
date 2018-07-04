298037ff525e191eb3f4dbf4247a2942747e856d
ty <tylergilland@gmail.com>
2018-07-03 23:09:12 -0600

    updates to creator store

--- Record_Request_App/Scripts/src/stores/CreatorStore/CreatorStore.ts
+++ Record_Request_App/Scripts/src/stores/CreatorStore/CreatorStore.ts
@@ -1,10 +1,13 @@
 import { RootStore } from "../RootStore/RootStore"
 import { computed, observable, action } from "mobx"
+import { SessionStore } from "../SessionStore"
 
 export class CreatorStore {
+    sessionStore: SessionStore
     constructor(private _root: RootStore) {}
     @action
     init = async () => {
+        this.sessionStore = this._root.sessionStore
         return
     }
 }
