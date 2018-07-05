5666dd636ef3998c51d75fb4bf7d51e351b1703a
BuildTools <unconfigured@null.spigotmc.org>
2018-07-05 11:59:30 -0600

    fixed typo

---
+++ Record_Request_App/Scripts/src/components/WarningDialog/WarningDialog.tsx
@@ -0,0 +1,44 @@
+import * as React from "react"
+import {
+    Dialog,
+    DialogFooter,
+    PrimaryButton,
+    DefaultButton,
+    DialogType
+} from "office-ui-fabric-react"
+import { RequestState } from "../../stores/RequestStore/RequestState";
+import { observer } from "mobx-react"
+
+export interface IWarningDialog {
+    requestState: RequestState
+}
+
+
+// ----------------------------------------------
+
+export const WarningDialog = observer((props: IWarningDialog) => {
+    return (
+        <Dialog
+        hidden={props.requestState.hiddenDialog}
+        onDismiss={() => props.requestState.removeParentBox(props.requestState.box)}
+        dialogContentProps={{
+          type: DialogType.normal,
+          title: 'Box or Folders',
+          subText:
+            `${props.requestState.msgBarMessage}`
+        }}
+        modalProps={{
+          titleAriaId: 'myLabelId',
+          subtitleAriaId: 'mySubTextId',
+          isBlocking: false,
+          containerClassName: 'ms-dialogMainOverride'
+        }}
+      >
+        {null /** You can also include null values as the result of conditionals */}
+        <DialogFooter>
+          <PrimaryButton onClick={() => props.requestState.removeChildFolders(props.requestState.box)} text="Yes" />
+          <DefaultButton onClick={() => props.requestState.removeParentBox(props.requestState.box)} text="No" />
+        </DialogFooter>
+      </Dialog>
+    )
+})
