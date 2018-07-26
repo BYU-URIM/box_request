bbddc681de9b655d90db4a4d833024a3b11c5ea2
Joshua-Jenkins33 <joshuabyu@yahoo.com>
2018-07-26 12:54:22 -0600

    ran linting

--- Record_Request_App/Scripts/test/FolderForm.test.ts
+++ Record_Request_App/Scripts/test/FolderForm.test.ts
@@ -10,7 +10,9 @@ test("Recreating a folder with the same name as an existing folder returns false
 test("If inputIsValid returns false, then return an error message unique to duplicate named folders", () => {
     folderForm.folderName = "folder 1"
     console.log(folderForm.inputIsValid)
-    expect(folderForm.errorMessage).toBe("Please use a Folder Name that does not already exist.")
+    expect(folderForm.errorMessage).toBe(
+        "Please use a Folder Name that does not already exist."
+    )
 })
 
 test("inputIsValid returns true if creating a new a folder with a different name than existing folders", () => {
@@ -31,7 +33,10 @@ test("inputIsValid returns false when creating a folder with a name longer than 
 })
 
 test("If inputIsValid returns false because of name length, then return an error message unique to overly long folder names", () => {
-    folderForm.folderName = "folder 00000000000000000000000000000000000000000000"
+    folderForm.folderName =
+        "folder 00000000000000000000000000000000000000000000"
     console.log(folderForm.inputIsValid)
-    expect(folderForm.errorMessage).toBe("The new folder name must be less than 50 characters.")
+    expect(folderForm.errorMessage).toBe(
+        "The new folder name must be less than 50 characters."
+    )
 })
