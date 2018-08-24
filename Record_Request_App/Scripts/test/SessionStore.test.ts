9c2773fa53a3ddbb31417c732416695bdb17fd6b
tyler gilland <tylergilland@gmail.com>
2018-08-24 16:59:04 -0600

    removed stale tests, added basic tests for uiStore

--- Record_Request_App/Scripts/test/SessionStore.test.ts
+++
@@ -1,7 +0,0 @@
-// import { sessionStore } from "."
-
-// test("Function only pulls departments 1 and 2, which the user has.", () => {
-//     expect(
-//         sessionStore.userDepartments.map(department => department.id)
-//     ).toEqual([1, 2])
-// })
---
+++ Record_Request_App/Scripts/test/UIStore.test.ts
@@ -0,0 +1,5 @@
+import { rootStore } from "."
+
+test("tests that the UIStore instance was initialized", () => {
+    expect(rootStore.uiStore.initialized)
+})
