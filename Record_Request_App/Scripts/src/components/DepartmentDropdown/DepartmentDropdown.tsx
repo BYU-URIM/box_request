43250ef17df46ea688eef5ca4060560014495e68
tyler gilland <tylergilland@gmail.com>
2018-08-27 15:50:14 -0600

    don't show a dropdown if they only have 1 department

--- Record_Request_App/Scripts/src/components/DepartmentDropdown/DepartmentDropdown.tsx
+++ Record_Request_App/Scripts/src/components/DepartmentDropdown/DepartmentDropdown.tsx
@@ -1,9 +1,8 @@
 import * as React from "react"
-import { Dropdown } from "office-ui-fabric-react"
+import { Dropdown, DocumentCardTitle } from "office-ui-fabric-react"
 import "./styles.scss"
 import { observer } from "mobx-react"
 import { IOption, IDropdownInfo } from "../../models"
-
 export interface IDepartmentDropdownProps {
     handleChanged(key: number): void
     options: Array<IOption>
@@ -14,17 +13,27 @@ export const DepartmentDropdown = observer(
     (props: IDepartmentDropdownProps) => {
         return (
             <div>
-                <h1 className="ms-font-xxl department-dropdown-center">
-                    {props.dropdownInfo.title}
-                </h1>
-                <Dropdown
-                    placeHolder={props.dropdownInfo.placeHolder}
-                    options={props.options}
-                    selectedKey={props.dropdownInfo.key}
-                    onChanged={(item: IOption) => props.handleChanged(item.key)}
-                    id={"dep_dropdown"}
-                    disabled={props.options.length === 1}
-                />
+                {props.options.length !== 1 ? (
+                    <>
+                        <h1 className="ms-font-xxl department-dropdown-center">
+                            {props.dropdownInfo.title}
+                        </h1>
+                        <Dropdown
+                            placeHolder={props.dropdownInfo.placeHolder}
+                            options={props.options}
+                            selectedKey={props.dropdownInfo.key}
+                            onChanged={(item: IOption) =>
+                                props.handleChanged(item.key)
+                            }
+                            id={"dep_dropdown"}
+                            disabled={props.options.length === 1}
+                        />
+                    </>
+                ) : (
+                    <DocumentCardTitle
+                        title={`Department: ${props.options[0].text}`}
+                    />
+                )}
             </div>
         )
     }
