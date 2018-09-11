6ad180139b49baba922250e50c0ac9422d296782
tyler gilland <tylergilland@gmail.com>
2018-09-11 14:12:20 -0600

    fixed node_env issues when testing

--- Record_Request_App/Scripts/src/env.ts
+++ Record_Request_App/Scripts/src/env.ts
@@ -6,21 +6,24 @@ export enum EnvType {
 
 // gloabal variable set at build time through build script
 // passed in through webpack plugin
-declare const NODE_ENV = "local"
+// tslint:disable-next-line:prefer-const
+let NODE_ENV: string
 
 // associates NODE_ENV string to Environment enum and checks for any uncrecognized NODE_ENV string
 // defaults to local if no NODE_ENV string is supplied by build script
-function getEnvironment(nodeEnv: string = "local"): EnvType {
+function getEnvironment(nodeEnv: string): EnvType {
     switch (nodeEnv) {
         case "local":
-            return EnvType.LOCAL
+        return EnvType.LOCAL
         case "sharepoint":
-            return EnvType.SHAREPOINT
+        return EnvType.SHAREPOINT
         default:
-            return EnvType.OTHER
+        return EnvType.LOCAL
     }
 }
 
 // make environment constant and publically available
 // export const ENVIRONMENT: EnvType = getEnvironment(NODE_ENV)
-export const ENVIRONMENT: EnvType = getEnvironment("local")
+console.log(NODE_ENV)
+
+export const ENVIRONMENT: EnvType = getEnvironment(NODE_ENV)
--- Record_Request_App/Scripts/webpack.config.js
+++ Record_Request_App/Scripts/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = function(env) {
                 warningsFilter: /export .* was not found in/,
             },
         },
-        devtool: "source-maps",
+        devtool: env.NODE_ENV === "local" ? "cheap-eval-source-maps" : "none",
         entry: "./src/main.tsx",
         output: {
             filename: "bundle.js",
