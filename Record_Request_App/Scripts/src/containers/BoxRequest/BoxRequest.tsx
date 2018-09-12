65dedd05e0f54e7e27ccf930a293c02cffb4bd80
tyler gilland <tylergilland@gmail.com>
2018-09-12 14:14:14 -0600

    updated Message component styling and structure

--- Record_Request_App/Scripts/src/containers/BoxRequest/BoxRequest.tsx
+++ Record_Request_App/Scripts/src/containers/BoxRequest/BoxRequest.tsx
@@ -1,17 +1,11 @@
 import * as React from "react"
 import {
     DepartmentDropdown,
-    SubmitModal,
-    CreateFolderModal,
     BoxList,
     FolderList,
     Checkout,
-    MsgBar,
-    CreateBoxModal,
 } from "../../components"
 import { inject, observer } from "mobx-react"
-import { ModalTypes } from "../../models"
-import { Modal } from "office-ui-fabric-react/lib/Modal"
 import { UIStore, CheckoutStore, User } from "../../stores"
 import "./styles.scss"
 
@@ -27,16 +21,6 @@ export class BoxRequest extends React.Component<{
         return (
             <>
                 <div className={"ms-Grid-row"}>
-                    <div className={"ms-Grid-col ms-sm12"}>
-                        {uiStore.message && (
-                            <MsgBar
-                                clearMessage={uiStore.clearMessage}
-                                message={uiStore.messageInfo}
-                            />
-                        )}
-                    </div>
-                </div>
-                <div className={"ms-Grid-row"}>
                     <div className={"ms-Grid-col ms-sm4 ms-smPush4"}>
                         <DepartmentDropdown
                             handleChanged={(id: number) =>
@@ -51,56 +35,23 @@ export class BoxRequest extends React.Component<{
                     </div>
                 </div>
                 <div className={"ms-Grid-row box-request-row"}>
-                    <div>
-                        <Modal
-                            isOpen={uiStore.modal !== ModalTypes.none}
-                            onDismiss={uiStore.clearModal}
-                            isBlocking={false}
-                            isDarkOverlay={false}
-                        >
-                            {uiStore.modal === ModalTypes.submit && (
-                                <SubmitModal
-                                    submit={uiStore.submitRequest}
-                                    modal={uiStore.modal}
-                                    requestForm={uiStore.requestForm}
-                                    close={uiStore.clearModal}
-                                />
-                            )}
-                            {uiStore.modal === ModalTypes.folder && (
-                                <CreateFolderModal
-                                    modal={uiStore.modal}
-                                    close={uiStore.clearModal}
-                                    box={userStore.selectedBox.BoxId}
-                                    folderForm={uiStore.folderForm}
-                                    createFolder={() => uiStore.createFolder()}
-                                />
-                            )}
-                            {uiStore.modal === ModalTypes.box && (
-                                <CreateBoxModal name={"testing"} />
-                            )}
-                        </Modal>
-                        <div>
-                            <div className={"ms-Grid-col ms-sm1"} />
-                            <BoxList
-                                initializeFolderForm={
-                                    uiStore.initializeFolderForm
-                                }
-                                dept={userStore.selectedDepartment}
-                            />
+                    <div className={"ms-Grid-col ms-sm1"} />
+                    <BoxList
+                        initializeFolderForm={uiStore.initializeFolderForm}
+                        dept={userStore.selectedDepartment}
+                    />
 
-                            <FolderList
-                                emptyMessage={
-                                    userStore.selectedDepartment &&
-                                    "Click on a box to view its folders"
-                                }
-                                box={userStore.selectedBox}
-                            />
-                            <Checkout
-                                dialogMessage={uiStore.dialogMessage}
-                                checkoutStore={checkoutStore}
-                            />
-                        </div>
-                    </div>
+                    <FolderList
+                        emptyMessage={
+                            userStore.selectedDepartment &&
+                            "Click on a box to view its folders"
+                        }
+                        box={userStore.selectedBox}
+                    />
+                    <Checkout
+                        dialogMessage={uiStore.dialogMessage}
+                        checkoutStore={checkoutStore}
+                    />
                 </div>
             </>
         )
--- Record_Request_App/Scripts/src/stores/UIStore/Message.ts
+++ Record_Request_App/Scripts/src/stores/UIStore/Message.ts
@@ -1,26 +1,36 @@
 import { MessageBarType } from "office-ui-fabric-react"
 
 export class Message {
-    name: MessageTypes
+    name: string
     text: string
     type: MessageBarType
     buttons: Array<string>
+    time: number = 0
+    constructor(msg: IMessage) {
+        Object.assign(this, msg)
+    }
+}
+export interface IMessage {
+    name: string
+    text: string
+    type: MessageBarType
+    buttons: Array<string>
+    time?: number
 }
 
-export type MessageTypes = "five-folders" | "cart-submit-success"
-
-export const messages: Array<Message> = [
-    {
+export const messages = {
+    Five_Folders: new Message({
         name: "five-folders",
         text:
             "Your request has 5 or more folders from the same box, we reccomend requesting the box instead instead of this many folders.",
         type: MessageBarType.info,
         buttons: [],
-    },
-    {
+    }),
+    Cart_Submit_Success: new Message({
         name: "cart-submit-success",
         text: "Thank you. Your order has been submitted.",
         type: MessageBarType.success,
         buttons: [],
-    },
-]
+        // time: 5000,
+    }),
+}
