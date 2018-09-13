09a152b38924745e7e2c1c95f5fb9abd30fedd35
tyler gilland <tylergilland@gmail.com>
2018-09-13 15:50:59 -0600

    cleaned up messages

--- Record_Request_App/Scripts/src/containers/Message/Message.tsx
+++ Record_Request_App/Scripts/src/containers/Message/Message.tsx
@@ -9,39 +9,16 @@ import "./styles.scss"
 export class Message extends React.Component<{ uiStore?: UIStore }> {
     render() {
         const { uiStore } = this.props
-        console.log(uiStore.message)
-
         return uiStore.message ? (
             <div className={"message-wrapper"}>
                 <div className={"message-content"}>
                     <MessageBar
                         messageBarType={uiStore.message.type}
                         isMultiline={true}
-                        dismissButtonAriaLabel="Close"
-                        onDismiss={uiStore.clearMessage}
-                        actions={
-                            <>
-                                {uiStore.message.buttons.map(_button => (
-                                    <MessageBarButton key={_button}>
-                                        {_button}
-                                    </MessageBarButton>
-                                ))}
-                            </>
-                        }
                         styles={{
-                            iconContainer: {
-                                fontSize: 18,
-                            },
-                            root: {
-                                alignItems: "center",
-                                verticalAlign: "center",
-                            },
                             text: {
                                 fontSize: 14,
                                 padding: 6,
-                            },
-                            actions: {
-                                margin: 0,
                             },
                         }}
                     >
--- Record_Request_App/Scripts/src/stores/UIStore/Message.ts
+++ Record_Request_App/Scripts/src/stores/UIStore/Message.ts
@@ -4,8 +4,8 @@ export class Message {
     name: string
     text: string
     type: MessageBarType
-    buttons: Array<string>
-    time: number = 0
+    buttons?: Array<string> = []
+    time?: number
     constructor(msg: IMessage) {
         Object.assign(this, msg)
     }
@@ -14,7 +14,7 @@ export interface IMessage {
     name: string
     text: string
     type: MessageBarType
-    buttons: Array<string>
+    buttons?: Array<string>
     time?: number
 }
 
@@ -24,13 +24,20 @@ export const messages = {
         text:
             "Your request has 5 or more folders from the same box, we reccomend requesting the box instead instead of this many folders.",
         type: MessageBarType.info,
-        buttons: [],
     }),
     Cart_Submit_Success: new Message({
         name: "cart-submit-success",
         text: "Thank you. Your order has been submitted.",
         type: MessageBarType.success,
-        buttons: [],
-        // time: 5000,
+    }),
+    NEW_FOLDER: new Message({
+        name: "folder-create-success",
+        text: "The Folder was created successfully.",
+        type: MessageBarType.success,
+    }),
+    NEW_BOX: new Message({
+        name: "box-create-success",
+        text: "The Box was created successfully.",
+        type: MessageBarType.success,
     }),
 }
