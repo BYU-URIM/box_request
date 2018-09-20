59b77554ac0f30e5527ff5f0307672848ad916bf
tyler gilland <tylergilland@gmail.com>
2018-09-20 15:36:43 -0600

    linting

--- Record_Request_App/Scripts/tasks/lint-styles.ts
+++ Record_Request_App/Scripts/tasks/lint-styles.ts
@@ -2,6 +2,4 @@ import * as shell from "shelljs"
 import { resolve } from "path"
 const base = resolve(".")
 shell.echo("\n----- linting and formatting styles -----\n")
-shell.exec(
-    `npx stylelint --fix "./src/**/*.scss"`
-)
+shell.exec(`npx stylelint --fix "./src/**/*.scss"`)
