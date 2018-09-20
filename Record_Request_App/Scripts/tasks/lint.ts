a90d4816667f256944896c778614f6e7383ba21b
tyler gilland <tylergilland@gmail.com>
2018-09-20 15:30:09 -0600

    updated tests stuff

--- Record_Request_App/Scripts/tasks/lint.ts
+++
@@ -1,9 +0,0 @@
-import * as shell from "shelljs"
-import { resolve } from "path"
-const base = resolve(".")
-shell.echo("\n----- formatting with prettier -----\n")
-shell.exec(
-    `npx prettier --write --config ${base}/.prettierrc ${base}/**/{**/*.ts*,'*.ts*'}`
-)
-shell.echo("\n----- linting with tslint -----\n")
-shell.exec(`npx tslint --fix -c '${base}/tslint.json' ${base}/**/*.ts*`)
