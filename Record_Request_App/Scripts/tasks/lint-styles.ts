42cf70ee96a5fa93919c2d19d8dfe2866768b2ef
tyler gilland <tylergilland@gmail.com>
2018-09-20 15:20:44 -0600

    setup style-lint

---
+++ Record_Request_App/Scripts/tasks/lint-styles.ts
@@ -0,0 +1,7 @@
+import * as shell from "shelljs"
+import { resolve } from "path"
+const base = resolve(".")
+shell.echo("\n----- linting and formatting styles -----\n")
+shell.exec(
+    `npx stylelint --fix "./src/**/*.scss"`
+)
