40bfec584f799307363a5435fec2b2505f1e4379
tyler gilland <tylergilland@gmail.com>
2018-10-11 11:32:35 -0600

    added tabs via pivot for submitting records

--- Record_Request_App/Scripts/src/containers/BoxRequest/BoxRequest.tsx
+++ Record_Request_App/Scripts/src/containers/BoxRequest/BoxRequest.tsx
@@ -6,9 +6,9 @@ import {
     Checkout,
 } from "../../components"
 import { inject, observer } from "mobx-react"
-import { UIStore, CheckoutStore, User } from "../../stores"
+import { UIStore, CheckoutStore, User, FormTypes } from "../../stores"
+import { Pivot, PivotItem, Label } from "office-ui-fabric-react"
 import "./styles.scss"
-import { FormTypes } from "../../models"
 
 @inject("uiStore", "checkoutStore", "userStore")
 @observer
@@ -29,7 +29,7 @@ export class BoxRequest extends React.Component<{
                                     .find(_dep => _dep.id === id)
                                     .select()
                             }
-                            options={userStore.userDepartmentsAsOptions}
+                            options={uiStore.userDepartmentsAsOptions}
                             dropdownInfo={uiStore.dropdownInfo}
                             initializeBoxForm={() =>
                                 (uiStore.form = FormTypes.NEW_BOX)
@@ -37,27 +37,46 @@ export class BoxRequest extends React.Component<{
                         />
                     </div>
                 </div>
-                <div className={"ms-Grid-row box-request-row"}>
-                    <div className={"ms-Grid-col ms-sm1"} />
-                    <BoxList
-                        initializeFolderForm={() =>
-                            (uiStore.form = FormTypes.NEW_FOLDER)
-                        }
-                        dept={userStore.selectedDepartment}
-                    />
-
-                    <FolderList
-                        emptyMessage={
-                            userStore.selectedDepartment &&
-                            "Click on a box to view its folders"
-                        }
-                        box={userStore.selectedBox}
-                    />
-                    <Checkout
-                        dialogMessage={uiStore.dialogMessage}
-                        checkoutStore={checkoutStore}
-                    />
-                </div>
+                {userStore.selectedDepartment && (
+                    <div className={"ms-Grid-row box-request-row"}>
+                        <Pivot
+                            styles={{
+                                root: {
+                                    display: "block",
+                                    textAlign: "center",
+                                    marginBottom: "32px",
+                                },
+                            }}
+                            onLinkClick={(a, b) => {
+                                console.log(a, b)
+                            }}
+                        >
+                            <PivotItem linkText="Request Records">
+                                <div className={"ms-Grid-col ms-sm1"} />
+                                <BoxList
+                                    initializeFolderForm={() =>
+                                        (uiStore.form = FormTypes.NEW_FOLDER)
+                                    }
+                                    dept={userStore.selectedDepartment}
+                                />
+                                <FolderList
+                                    emptyMessage={
+                                        userStore.selectedDepartment &&
+                                        "Click on a box to view its folders"
+                                    }
+                                    box={userStore.selectedBox}
+                                />
+                                <Checkout
+                                    dialogMessage={uiStore.dialogMessage}
+                                    checkoutStore={checkoutStore}
+                                />{" "}
+                            </PivotItem>
+                            <PivotItem linkText="Submit Records">
+                                <div className={"ms-Grid-col ms-sm1"} />
+                            </PivotItem>
+                        </Pivot>
+                    </div>
+                )}
             </>
         )
     }
