3df3449f5fc1044d3d01b31ce44bea1aa9df6756
tyler gilland <tylergilland@gmail.com>
2019-04-30 19:21:02 -0600

    storybook

---
+++ Record_Request_App/Scripts/src/components/Greeting/greeting.story.tsx
@@ -0,0 +1,35 @@
+// import * as React from "react"
+
+// import { storiesOf } from "@storybook/react"
+// import { text, select, boolean } from "@storybook/addon-knobs/react"
+
+// storiesOf("components/Greeting", module).addWithJSX(
+//     "basic PrimaryButton",
+//     () => (
+//         <button
+//             label={text("label", "Enroll")}
+//             disabled={boolean("disabled", false)}
+//             onClick={() => alert("hello there")}
+//         />
+//     ),
+//     {
+//         info: {links
+//             text: `
+
+//   ### Notes
+
+//   light button seen on <https://zpl.io/aM49ZBd>
+
+//   ### Usage>
+//   ~~~js
+//   <button
+//     label={text('label', 'Enroll')}
+//     disabled={boolean('disabled',false)}
+//     onClick={() => alert('hello there')}
+//   />
+//   ~~~
+
+// `,
+//         },
+//     }
+// )
